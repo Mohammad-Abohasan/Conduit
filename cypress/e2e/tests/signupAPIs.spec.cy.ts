d7f962990e7812ba81755efc015ca09751b463b2
Mohammad-Abohasan <mo7ammad.saed@gmail.com>
2023-10-06 21:53:36 +0300

    try `POST` and `get` via apiCall (custom command)

--- cypress/e2e/tests/signupAPIs.spec.cy.ts
+++ cypress/e2e/tests/signupAPIs.spec.cy.ts
@@ -1,6 +1,8 @@
 import { generateRandomString } from "../../support/helpers/generateHelper";
 import addUser from "../../support/helpers/signupHelper";
 
+const baseUrl = Cypress.config().baseUrl;
+
 describe("Conduit: Signup", () => {
   it("Cxxx1: Conduit - Create new Account", () => {
     addUser.addNewUserUsingAPI(
@@ -19,5 +21,20 @@ describe("Conduit: Signup", () => {
       },
     };
     addUser.addNewUserUsingAPI2(payload);
+  });
+
+  it("Cxxx3: Conduit - Create new Account", () => {
+    const payload = {
+      user: {
+        username: `mohammad${generateRandomString()}`,
+        email: `mohammad${generateRandomString()}@gmail.com`,
+        password: "mohammad123",
+      },
+    };
+    cy.apiCall("POST", `${baseUrl}/api/users`, payload);
+  });
+
+  it("Cxxx4: Conduit - View all articles", () => {
+    cy.apiCall("GET", `${baseUrl}/api/articles`);
   });
 });
