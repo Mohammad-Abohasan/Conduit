55a2c6b8340acbe6fb3c6bc9af9bb830a0838f26
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-10-24 17:05:50 +0300

    rename file

---
+++ cypress/support/helpers/genericHelper.ts
@@ -0,0 +1,12 @@
+export default class GenericHelper {
+  static generateRandomString() {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < Math.round(Math.random() * 10); i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+    return result;
+  }
+}
